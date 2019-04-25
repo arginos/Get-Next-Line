d272d61b0ab5267393d63ce8ceb63c5ade06493d
Argin Simonian <asimonia@e1z2r9p28.42.us.org>
2019-04-24 17:12:08 -0700

    first push

---
+++ Public/get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asimonia <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/30 11:06:30 by asimonia          #+#    #+#             */
+/*   Updated: 2018/12/12 17:41:28 by asimonia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include "libft/libft.h"
+# include <stdlib.h>
+# include <unistd.h>
+
+# define BUFF_SIZE 4
+
+int	get_next_line(const int fd, char **line);
+
+#endif
